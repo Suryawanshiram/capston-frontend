19f21cccb19a0a8dc3f69894498d99e764cab70f
Ram B. Suryawanshi <101462022+Suryawanshiram@users.noreply.github.com>
2023-10-30 14:35:27 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
-
 const Signup = () => {
   const navigate = useNavigate();
   const [name, setName] = useState("");
